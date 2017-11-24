91acfbbe0dbfec1274df46b0d96e9252183f506b
lvfuan@163.com <lvfuan@163.com>
2017-11-24 18:25:42 +0800

    1

---
+++ An.DotNetDemo/An.MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using An.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace An.MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var url= AppDomain.CurrentDomain.BaseDirectory.Replace("bin\\Debug\\", "")+ "test.config";
+            var t = ConfigHelper.GetConfig(url, "test");
+            return View();
+        }
+    }
+}
