164648feb5a640936bc4a67bbaf2fc315eb7a091
lvfuan@163.com <lvfuan@163.com>
2017-11-27 18:05:35 +0800

    init

---
+++ An.DotNetDemo/An.MVCDemo/Controllers/LogController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WriteErrorLogSendEmail;
+namespace An.MVCDemo.Controllers
+{
+    public class LogController : Controller
+    {
+        // GET: Log
+        public ActionResult Index()
+        {
+            Log.Debug("1111111111");
+            return View();
+        }
+    }
+}
--- An.DotNetDemo/UnitTestConsole/Program.cs
+++ An.DotNetDemo/UnitTestConsole/Program.cs
@@ -5,17 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 using An.Common;
 using System.Configuration;
+using WriteErrorLogSendEmail;
+using System.Net;
+using System.Net.Mail;
+
 namespace UnitTestConsole
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            MailHelper.SendEmail("403062955@qq.com", "测试", "Hello World");
+            //Log.Debug("1111111111");
             #region 自定义配置文件读取调用方法
-            var u = AppDomain.CurrentDomain.BaseDirectory.Replace("bin\\Debug\\", "") + "CustomerConfig.config";//获取当前项目的路径配置文件
-            var url = ConfigurationManager.AppSettings["url"];//读取配置文件配置的url
-            var t = ConfigHelper.GetConfig(u, "test");
+            //var u = AppDomain.CurrentDomain.BaseDirectory.Replace("bin\\Debug\\", "") + "CustomerConfig.config";//获取当前项目的路径配置文件
+            //var url = ConfigurationManager.AppSettings["url"];//读取配置文件配置的url
+            //var t = ConfigHelper.GetConfig(u, "test");
             #endregion
         }
     }
-}
+}   
